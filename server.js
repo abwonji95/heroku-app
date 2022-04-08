55637155691dd06cc6c1efd70a8c2ef09105b8f6
abwonji95 <abwonji95@gmail.com>
2022-04-08 22:13:46 +0300

    updated

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
-const express = require('express');
-const path = require('path');
-const app = express();
-app.use(express.static(__dirname + '/dist/central-admin'));
-app.get('/*', function(req,res)
+function requireHTTPS(req, res, next) {
+  // The 'x-forwarded-proto' check is for Heroku
+  if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
+      return res.redirect('https://' + req.get('host') + req.url);
+  }
+  next();
+}
 
-{res.sendFile(path.join(__dirname+'/dist/central-admin/index.html'));});
+const express = require('express');
+const app = express();
+
+app.use(requireHTTPS);
+app.use(express.static('./dist/central_admin'));
+
+app.get('/*', (req, res) =>
+  res.sendFile('index.html', {root: 'dist/central_admin/'}),
+);
 
 app.listen(process.env.PORT || 8080);
+''
